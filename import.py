f9feab36003e060e73864f75ee6d28fc8e1a346b
Anusuriya Devaraju <anusuriya_asd@yahoo.com>
2019-09-25 12:10:25 +0200

    modified log dir

--- import.py
+++ import.py
@@ -325,7 +325,7 @@ def initLog():
     logger = logging.getLogger('pg_itis_importer')
     logger.setLevel(logging.DEBUG)
     # create file handler which logs even debug messages
-    fh = logging.FileHandler(cwd+'\data\log\itis.log')
+    fh = logging.FileHandler(cwd+'/data/itis.log')
     fh.setLevel(logging.DEBUG)
     # create console handler with a higher log level
     ch = logging.StreamHandler()
